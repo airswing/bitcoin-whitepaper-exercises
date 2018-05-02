66934cd8c56040e844159d699b5a4caea99b97ff
Elias <elias3c@gmail.com>
2018-05-02 11:21:50 -0500

    Implement block generation and verification

--- hashing/hashing.js
+++ hashing/hashing.js
@@ -28,10 +28,19 @@ Blockchain.blocks.push({
 });
 
 // TODO: insert each line into blockchain
-// for (let line of poem) {
-// }
+for (let line of poem) {
+	var block = {
+		index: Blockchain.blocks.length,
+		data: line,
+		timestamp: Date.now(),
+		prevHash: Blockchain.blocks[Blockchain.blocks.length-1].hash
+	};
+	block.hash = blockHash(block)
+	console.log(block)
+	Blockchain.blocks.push(block)
+}
 
-// console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
+console.log(`Blockchain is valid: ${verifyChain(Blockchain)}`);
 
 
 // **********************************
@@ -39,5 +48,24 @@ Blockchain.blocks.push({
 function blockHash(bl) {
 	return crypto.createHash("sha256").update(
 		// TODO: use block data to calculate hash
+		bl.index + bl.hash + bl.data + bl.timestamp + bl.prevHash	
 	).digest("hex");
+}
+
+function verifyChain(chain) {
+	for(var i = 1; i < chain.blocks.length; i++) {
+		if(chain.blocks[i].prevHash !== chain.blocks[i-1].hash){
+			console.log('Invalid block at index:' + i + ' hash comparison failed with previous block')
+			return false;
+		}
+		if(chain.blocks[i].timestamp < chain.blocks[i-1].timestamp){
+			console.log('Invalid block at index:' + i + ' miner has no concept of the order of time')
+			return false;
+		}
+		if(chain.blocks[i].data !== poem[i-1]) {
+			console.log('Invalid block at index:' + i + ' Tupacs poem is invalid')
+			return false;	
+		}
+	}
+	return true;
 }
